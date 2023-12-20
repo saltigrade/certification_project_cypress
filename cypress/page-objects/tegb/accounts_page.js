6afdf18992a15dbe98ee9fed68fc668ca6fa50d8
Sabina Parikova <hamanovas@gmail.com>
2023-12-20 19:31:20 +0100

    trying create DDT, it pass only with cy.log, hard task to unify BE and FE, at least json file looks ok

--- cypress/page-objects/tegb/accounts_page.js
+++ cypress/page-objects/tegb/accounts_page.js
@@ -1,8 +1,24 @@
 export class AccountsPage {
-  constructor() {}
+  constructor() {
+    this.accountNumber = "td[data-testid='account-number']";
+    this.accountBalance = "td[data-testid='account-balance']";
+    this.accountType = "td[data-testid='account-type']";
+  }
 
   waitForAccountsApi() {
     cy.wait("@accounts_api").its("request.method").should("eq", "GET");
+    return this;
+  }
+  selectAccountNumber(number) {
+    cy.get(this.accountNumber).type(number);
+    return this;
+  }
+  selectAccountBalance(startBalance) {
+    cy.get(this.accountBalance).type(startBalance);
+    return this;
+  }
+  selectAccountType(type) {
+    cy.get(this.accountType).type(type);
     return this;
   }
 }
